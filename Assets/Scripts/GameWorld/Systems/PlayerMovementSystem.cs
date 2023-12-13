c41e4ad0903f35223dbf01ab40878ea160ce712c
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2023-12-13 10:43:27 +0800

    Basic player movement (#5)
    * CreateFolder
    
    * chore: add unity.physics package
    
    * feat: add new input system
    
    * Major reorganization and refactorization (#4)
    
    * create testing folder
    
    - only for testing purposes
    
    * enemy spawn
    
    - not yet set the spawn range, but now all entities can only spawn at one place
    
    * feat: enemy now can spawn random in place
    
    * DestroyEntity0.1
    
    * chore: enemy spawn scripts naming convention
    
    * chore: naming convention modify
    
    * chore: major reorganization and refactorization
    
    * chore: change all var to types
    
    ---------
    
    Co-authored-by: CallMeWho <lewxiuzhen@gmail.com>
    Co-authored-by: DanyYap <124513296+DanyYap@users.noreply.github.com>
    
    * feat: add color materials
    
    * feat: add unity physics sample
    
    * feat: add UnityUtil package
    
    * feat: change hdrp volume & shadow settings
    
    * feat: add user input system
    
    * feat: add OnCreate for PlayerMovementSystem
    
    * feat: simple player movement
    
    * feat: add simple dash system
    
    * chore: Dispose Controls in OnDestroy method
    
    * feat: use SystemAPIQueryBuilder instead
    
    ---------
    
    Co-authored-by: DanyYap <124513296+DanyYap@users.noreply.github.com>
    Co-authored-by: CallMeWho <lewxiuzhen@gmail.com>

---
+++ Assets/Scripts/GameWorld/Systems/PlayerMovementSystem.cs
@@ -0,0 +1,71 @@
+using Unity.Mathematics;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Physics;
+
+[UpdateAfter(typeof(InputSystem))]
+public partial struct PlayerMovementSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<UserInputSingleton>();
+
+        // Player with Speed component attached to it
+        EntityQuery query = SystemAPI.QueryBuilder()
+                .WithAll<Tag_Player, PhysicsDamping, Speed>()
+                .WithAllRW<PhysicsVelocity>()
+                .WithAllRW<SecondaryVelocity, Dash>()
+                .Build();
+
+        state.RequireForUpdate(query);
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        UserInputSingleton userInput = SystemAPI.GetSingleton<UserInputSingleton>();
+
+        foreach (
+            var (velocity, damping, secondaryVelocity, speed, dash) in
+            SystemAPI.Query<
+                RefRW<PhysicsVelocity>,
+                RefRO<PhysicsDamping>,
+                RefRW<SecondaryVelocity>,
+                RefRO<Speed>,
+                RefRW<Dash>
+            >().WithAll<Tag_Player>()
+        )
+        {
+            // Overwrite velocity with the user's MoveAxis input
+            velocity.ValueRW.Linear = new float3(
+                userInput.MoveAxis.x, 0.0f, userInput.MoveAxis.y
+            ) * speed.ValueRO.Value;
+
+            float3 v = secondaryVelocity.ValueRW.Value;
+
+            // Only update the direcion if the user is moving
+            if (math.lengthsq(userInput.MoveAxis) > 0.0f)
+            {
+                dash.ValueRW.Direction = userInput.MoveAxis;
+            }
+
+            if (userInput.Dash)
+            {
+                // Calculate dash velocity
+                v += new float3(
+                    dash.ValueRO.Direction.x, 0.0f, dash.ValueRO.Direction.y
+                ) * dash.ValueRO.Value;
+            }
+
+            // TODO: Move this computation out of this system (it can be independant)
+            // Manually damp secondary velocity
+            v -= v * math.min(1.0f, damping.ValueRO.Linear * SystemAPI.Time.DeltaTime);
+
+            // Update velocity value for the next frame
+            secondaryVelocity.ValueRW.Value = v;
+            // Add the resultant secondary velocity to the physics linear velocity
+            velocity.ValueRW.Linear += v;
+        }
+    }
+}
---
+++ Packages/UnityUtil/Voxell.Util/InspectorAttributes.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Voxell.Util
+{
+    public class InspectOnly : PropertyAttribute { }
+    public class Scene : PropertyAttribute { }
+}
