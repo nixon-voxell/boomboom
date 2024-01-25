03cfc73e0c881f4ef8ccb7b0aeee8bf5a0ea8e01
CallMeWho <lewxiuzhen@gmail.com>
2024-01-25 12:28:56 +0800

    feat: setting ui audio and back button function

--- Assets/Scripts/GameWorld/Mono/UserInterface/StartMenuMono.cs
+++ Assets/Scripts/GameWorld/Mono/UserInterface/StartMenuMono.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -14,12 +15,21 @@ public class StartMenuMono : UiMono
         this.m_QuitBtn = this.Root.Query<Button>("quit-btn");
 
         this.m_StartBtn.clicked += this.StartGame;
+        this.m_SettingsBtn.clicked += this.OpenSettings;
         this.m_QuitBtn.clicked += this.QuitGame;
     }
 
     private void StartGame()
     {
         GameStateSpawnerMono.Instance.SetTargetState(GameState.InGame);
+    }
+
+    private void OpenSettings()
+    {
+        /* SetOnlyVisible: alr include hide other ui, and only show the ui mentioned,
+         so no need to add "this.Root.visible = false;" */
+
+        UiManagerMono.Instance.SetOnlyVisible(typeof(SettingsMono));   //show settings menu only, and hide other ui
     }
 
     private void QuitGame()
